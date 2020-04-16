dec658eb189bb13c80e0b03ea91ebca4fc70bb52
ramy <ramy1415@hotmail.com>
2020-04-16 18:11:55 +0200

    adding gooogle auth2 - font awesome and some styling in buttons

--- Gemfile
+++ Gemfile
@@ -58,3 +58,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 gem 'devise-bootstrap-views', '~> 1.0'
 gem 'omniauth-facebook'
+gem 'omniauth-google-oauth2'
+gem 'activerecord-session_store'
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,:omniauthable, omniauth_providers: %i[facebook]
+         :recoverable, :rememberable, :validatable,:omniauthable, omniauth_providers: [:facebook,:google_oauth2]
   validates :username, :presence => true
   has_one_attached :local_avatar
   
