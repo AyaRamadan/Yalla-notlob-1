6bda612999363e726c2e4101225499bbab450f75
ramy1415 <ramy1415@hotmail.com>
2020-04-19 18:44:05 +0200

    adding status column to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,5 @@ class Order < ApplicationRecord
   belongs_to :user
   has_one_attached :menu
   enum category: [:breakfast, :dinner, :lunch]
+  enum status: [:waiting, :finished, :canceled]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_19_045904) do
+ActiveRecord::Schema.define(version: 2020_04_19_163207) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2020_04_19_045904) do
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "status", default: 0
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
